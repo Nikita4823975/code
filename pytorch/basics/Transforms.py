e90bfb882c915a50df6741cb11ec7211f94ee3f4
Nik <nikita.buharinov@mail.ru>
2024-10-18 13:32:36 +0300

    Transforms

---
+++ pytorch/basics/Transforms.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+)
+
+train_dataloader = DataLoader(ds, batch_size=64, shuffle=True)
+train_features, train_labels = next(iter(train_dataloader))
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
